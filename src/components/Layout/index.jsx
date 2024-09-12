4ac7fa1b35ac4bbe78ddf6c68419a22de9a70e9d
johahert <johahert102@student.kau.se>
2024-09-12 11:41:15 +0200

    färg

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import bghue from '../../assets/bghuey.svg'
 const Layout = ({themeColor}) =>{
     return (
-        <div className="layout bg-gradient-to-b from-gray-200 to-gray-400">
+        <div className="layout bg-gradient-to-br from-teal-700 to-teal-950">
 
             {/* <div className='absolute   inset-0 box-border'>
                 <img src={bghue} className='block w-full h-36 object-center object-cover' alt="" />
